a19d4fc3fbcf0dee5e693f5e2f7708f8b703dd64
Chocoeater <lonelypain98@gmail.com>
2025-08-03 14:00:23 +0300

    add user model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True, verbose_name="E-mail", help_text="Введите адрес электронной почты")
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='имя пользователя',
+        help_text='Введите имя пользователя'
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+
+    def __str__(self):
+        return f'{self.username} -- {self.email}'
