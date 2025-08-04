494afb0b5d1c978eae8ec0c1bfb8438fd6cca774
Chocoeater <lonelypain98@gmail.com>
2025-08-04 16:37:28 +0300

    add serializers and perms for users

---
+++ users/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    """Разрешение на редактирование только для владельца или админа"""
+    def has_object_permission(self, request, view, obj):
+        # Разрешаем GET, HEAD, OPTIONS для всех аутентифицированных
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Для остальных методов проверяем владельца или админа
+        return obj == request.user or request.user.is_staff
