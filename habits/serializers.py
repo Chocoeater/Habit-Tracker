7ca8e8e51adc0198c746f2330a2eb9d2f9d74142
Chocoeater <lonelypain98@gmail.com>
2025-08-04 15:09:41 +0300

    add serializers and perms

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from habits.models import Habit
 from habits import validators
@@ -7,7 +7,8 @@ from habits import validators
 class MyHabitSerializer(ModelSerializer):
     class Meta:
         model = Habit
-        fields = ['place', 'time_to_do', 'action', 'frequency', 'tag_pleasant_habit', 'linked_habit', 'reward', 'duration', 'tag_public']
+        fields = ['place', 'time_to_do', 'action', 'frequency', 'tag_pleasant_habit', 'linked_habit', 'reward',
+                  'duration', 'tag_public']
 
     def validate(self, attrs):
         validators.validate_mutually_exclusive_fields(attrs)
@@ -15,3 +16,16 @@ class MyHabitSerializer(ModelSerializer):
         validators.validate_duration(attrs)
         validators.validate_frequency(attrs)
         return attrs
+
+
+class PublicHabitSerializer(ModelSerializer):
+    owner = SerializerMethodField()
+
+    def get_owner(self, obj):
+        return obj.user.username
+
+    class Meta:
+        model = Habit
+        fields = ['owner', 'place', 'time_to_do', 'action', 'frequency', 'tag_pleasant_habit', 'duration']
+        read_only_fields = fields
+
--- habits/views.py
+++ habits/views.py
@@ -1,12 +1,29 @@
-
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from habits.models import Habit
 from habits.paginators import MyPaginator
-from habits.serializers import MyHabitSerializer
+from habits.permissions import IsOwnerOrAdmin
+from habits.serializers import MyHabitSerializer, PublicHabitSerializer
 
 
 class HabitViewSet(ModelViewSet):
-    queryset = Habit.objects.all()
-    serializer_class = MyHabitSerializer
     pagination_class = MyPaginator
+    serializer_class = MyHabitSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Habit.objects.all()
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PublicHabitViewSet(ReadOnlyModelViewSet):
+    pagination_class = MyPaginator
+    serializer_class = PublicHabitSerializer
+
+    def get_queryset(self):
+        return Habit.objects.filter(tag_public=True)
